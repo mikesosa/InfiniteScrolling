1eda3b6fcf39b0c22f4e1d14b65d4e6ad84b3153
mikesosa <michael.sosa@holbertonschool.com>
2020-02-04 14:13:58 -0500

    Updates

---
+++ precache-manifest.3c158508208daf2c7fd874c460efb6a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99383677b8ef9f38c060b90864194481",
+    "url": "/InfiniteScrolling/index.html"
+  },
+  {
+    "revision": "2ce9ddabcb7de8608f9d",
+    "url": "/InfiniteScrolling/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "71bd8b26fc6677f2d7d1",
+    "url": "/InfiniteScrolling/static/css/main.976e7528.chunk.css"
+  },
+  {
+    "revision": "2ce9ddabcb7de8608f9d",
+    "url": "/InfiniteScrolling/static/js/2.55aff78a.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/InfiniteScrolling/static/js/2.55aff78a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71bd8b26fc6677f2d7d1",
+    "url": "/InfiniteScrolling/static/js/main.6a5ff927.chunk.js"
+  },
+  {
+    "revision": "bf0d49eaa4fe82482ee3",
+    "url": "/InfiniteScrolling/static/js/runtime-main.d3810be8.js"
+  }
+]);
